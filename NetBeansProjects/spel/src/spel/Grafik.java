8a1c5da9ac1385890437a869b5b932653d15f9ed
pontepers <pontus.persson@elev.it-gymnasiet.se>
2016-10-12 11:14:34 +0200

    move netbeans

---
+++ NetBeansProjects/spel/src/spel/Grafik.java
@@ -0,0 +1,21 @@
+package spel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Grafik extends JFrame {
+
+        public Grafik(){
+    setSize(700,800); //x, y
+    setLocation(300,300);
+    setDefaultCloseOperation(3);
+    setTitle("420blazeitm8");
+    setResizable(false);
+    setVisible(true);
+        }
+
+    public static void main (String[] args){
+   
+
+    }
+}
